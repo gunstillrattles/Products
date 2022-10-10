540ce13cb69a9c145ac48477cf35aa0d149538d5
gunstillrattles <marlen.mukashev@narxoz.kz>
2022-10-10 10:55:07 +0600

    Initial commit

--- database/migrations/2022_10_05_232708_create_users_table.php
+++ database/migrations/2022_10_05_232708_create_users_table.php
@@ -2,12 +2,12 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-class CreateAuthorizationTable extends Migration
+class CreateUsersTable extends Migration
 {
     public function up()
     {
-        Schema::create('authorization', function (Blueprint $table) {
-            $table->bigIncrements('userId');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
             $table->string('login');
             $table->string('password');
             $table->string('name');
